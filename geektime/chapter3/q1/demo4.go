0573e09e52267f0700a80a4675bb494b6a13fa9f
youyifan_xforceplus.com <youyifan@xforceplus.com>
2018-11-11 21:38:35 +0800

    chapter3

---
+++ geektime/chapter3/q1/demo4.go
@@ -0,0 +1,16 @@
+package q1
+
+import (
+"flag"
+)
+
+var name string
+
+func init() {
+	flag.StringVar(&name, "name", "everyone", "The greeting object.")
+}
+
+func main() {
+	flag.Parse()
+	hello(name)
+}
---
+++ geektime/chapter3/q1/demo4_lib.go
@@ -0,0 +1,7 @@
+package q1
+
+import "fmt"
+
+func hello(name string) {
+	fmt.Printf("Hello, %s\n", name)
+}
